90cdd6ea669433cf1d082750028c3240893e63fb
haru <gfxmagicz@gmail.com>
2024-10-29 08:58:28 -0400

    Update text_utils_main.py
    Fixed it to include aliases woops

--- text_utils_main.py
+++ text_utils_main.py
@@ -1,10 +1,10 @@
-import text_utils
+import text_utils as tu
 choice_upper = input("Enter whether you want to A reverse your string or B capitalize your string!: ")
 choice = choice_upper.lower()
 
 if choice == "a":
     s = input("What set of string would you like to reverse?: ")
-    text_utils.reverse_string(s)
+    tu.reverse_string(s)
 elif choice == "b":
     s = input("What set of string would you like to capitalize?: ")
-    text_utils.capitalize_string(s)
+    tu.capitalize_string(s)
